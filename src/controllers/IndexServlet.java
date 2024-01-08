239dec7800efe2ee99c4d30cdfb5880240695dba
risa.yasuda <Risa_Yasuda@sis2.saison.co.jp>
2024-01-08 20:18:00 +0900

    バリデーションを追加

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -57,6 +57,7 @@ public class IndexServlet extends HttpServlet {
         request.setAttribute("tasks", tasks);
         request.setAttribute("tasks_count", tasks_count); // 全件数
         request.setAttribute("page", page); // ページ数
+
         // フラッシュメッセージがセッションスコープにセットされていたら
         // リクエストスコープに保存する（セッションスコープからは削除）
         if (request.getSession().getAttribute("flush") != null) {
